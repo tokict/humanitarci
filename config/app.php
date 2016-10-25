29b789166ae182b3bb64270caf7e0468aae3b50f
Tino Tokic <tino.tokic@credissimo.bg>
2016-10-25 14:50:26 +0300

    Added base models

--- config/app.php
+++ config/app.php
@@ -156,6 +156,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Terranet\Administrator\ServiceProvider::class,
+        \Reliese\Coders\CodersServiceProvider::class,
 
     ],
 
