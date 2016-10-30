9cc1f5a5488933c7adde363cf2fc54bc36fa4448
tino <tino@tokic.com.hr>
2016-10-30 10:38:43 +0200

    Landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,45 +1,369 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <title>Laravel</title>
+<!-- saved from url=(0051) -->
+<html class="no-mobile no-touch" style="height: auto;">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>Humanitarci.hr</title>
+    <meta name="description"
+          content="Humanitarci.hr je potpuno transparentna platforma za doniranje novca, hrane i nuznih potrepstina">
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+    <!--[if IE]>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'><![endif]-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
 
-        <style>
-            html, body {
-                height: 100%;
-            }
+    <!-- Favicons -->
+    <link rel="shortcut icon" href="http://rhythm.bestlooker.pro/images/favicon.png">
+    <link rel="apple-touch-icon" href="http://rhythm.bestlooker.pro/images/apple-touch-icon.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="http://rhythm.bestlooker.pro/images/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="114x114"
+          href="http://rhythm.bestlooker.pro/images/apple-touch-icon-114x114.png">
 
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
+    <!-- CSS -->
+    <link rel="stylesheet" href="front/css/bootstrap.min.css">
+    <link rel="stylesheet" href="front/css/style.css">
+    <link rel="stylesheet" href="front/css/style-responsive.css">
+    <link rel="stylesheet" href="front/css/animate.min.css">
+    <link rel="stylesheet" href="front/css/vertical-rhythm.min.css">
+    <link rel="stylesheet" href="front/css/owl.carousel.css">
+    <link rel="stylesheet" href="front/css/magnific-popup.css">
+    <style type="text/css">
+        .accent {
+            color: #08c;
+        }
+    </style>
 
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
+<body class="appear-animate">
 
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
+<!-- Page Wrap -->
+<div class="page" id="top">
 
-            .title {
-                font-size: 96px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Laravel 5</div>
+    <!-- Navigation panel -->
+    <div id="sticky-wrapper" class="sticky-wrapper" style="height: 75px;">
+        <nav class="main-nav js-stick" style="width: 1840px;">
+            <div class="container relative clearfix">
+                <!-- Logo ( * your text or image into link tag *) -->
+                <div class="nav-logo-wrap local-scroll">
+                    <a href="#top" class="logo">
+                        <!-- <img src="./landing_files/logo-dark.png" alt=""> -->
+                    </a>
+                </div>
+                <div class="mobile-nav" style="height: 75px; line-height: 75px; width: 75px;">
+                    <i class="fa fa-bars"></i>
+                </div>
+                <!-- Main Menu -->
+                <div class="inner-nav desktop-nav">
+                    <ul class="clearlist scroll-nav local-scroll">
+                        <li><a href="#about" style="height: 75px; line-height: 75px;" class="">O nama</a></li>
+                        <!-- Button -->
+                        <li>
+                            <a href="#subscribe" style="height: 75px; line-height: 75px;"><span
+                                        class="btn btn-mod btn-circle"><i
+                                            class="fa fa-shopping-cart"></i> Doniraj</span></a>
+                        </li>
+                        <!-- End Button -->
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+    <!-- End Navigation panel -->
+
+    <!-- Home Section -->
+    <section class="page-section pb-0 bg-dark" id="home"
+             style="background-image: url(&quot;/front/images/djeca.jpg&quot;); background-position: 0px -300px">
+        <div class="relative">
+
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12 mb-50">
+                        <div class="align-center">
+                            <h1 class="hs-line-8 no-transp font-alt mb-40 mb-xs-20">Jedina potpuno transparentna
+                                platforma za doniranje</h1>
+                            <h2 class="hs-line-15 font-alt">Pomozimo onima kojima je pomoć<br>najpotrebnija</h2>
+                        </div>
+                        <div align="center">
+                            <!-- Timer -->
+                            <ul class="countdown dark clearlist clearfix mb-80 mb-xs-20"
+                                data-finish-date="12/1/2016 12:00:00" data-UTC="2"
+                                data-finish-message="We have already started!">
+                                <li>
+                                    <span class="countdown-number"><span class="days font-alt">00</span><b
+                                                class="days_ref">dana</b></span>
+                                </li>
+                                <li>
+                                    <span class="countdown-number"><span class="hours font-alt">00</span><b
+                                                class="hours_ref">sati</b></span>
+                                </li>
+                                <li>
+                                    <span class="countdown-number"><span class="minutes font-alt">00</span><b
+                                                class="minutes_ref">minuta</b></span>
+                                </li>
+                                <li>
+                                    <span class="countdown-number"><span class="seconds font-alt">00</span><b
+                                                class="seconds_ref">sekundi</b></span>
+                                </li>
+                            </ul>
+                            <!-- End Timer -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    </section>
+    <!-- End Home Section -->
+
+
+    <!-- Text Section -->
+    <section class="small-section" id="about">
+        <div class="container relative">
+
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+                    <blockquote class="mb-0 mt-0">
+                        <p>
+                            Sve je više gladnih, ljudi kojima je zimi hladno jer nemaju za ogrjev.<br>
+                            Rado biste pomogli, ali bojite da će Vas novac zavrsiti u džepovima ljudi koji koriste Vašu
+                            dobrotu za vlastitu korist.<br>
+                        <p class="accent">Ovdje je sve transparentno - donacije, nabavka, troškovi, isporuka.</p>
+                        </p>
+                    </blockquote>
+                </div>
             </div>
         </div>
-    </body>
+
+</div>
+</section>
+<!-- End Text Section -->
+
+<!-- About Section -->
+<section class="tiny-section" id="about">
+    <div class="container relative">
+
+
+        <div class="row">
+            <div class="col-sm-10 col-sm-offset-1">
+
+                <h2 class="section-title font-alt mb-70 mb-sm-40">O nama</h2>
+
+                <div class="row">
+                    <div class="col-sm-6">
+                        <div class="section-text mb-70 mb-xs-40">
+                            <span class="dropcap font-alt">P</span>otaknuti čestim novinskim člancima o ljudima koji
+                            žive u strašnim uvjetima, kao i nemogućnošću da lako doniramo ili saznamo kome je pomoć
+                            trenutno najpotrebnija, organizirali smo se da napravimo platformu za doniranje.<br>Gotovo
+                            nitko od tima koji se spontano organizirao preko društvenih mreža nije se ranije poznavao.
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="section-text mb-70 mb-xs-40">
+                            <span class="dropcap font-alt">M</span>i smo tim sastavljen od programera, dizajnera i
+                            članova humanitarnih udruga. Ovaj projekt je potpuno otvoren i sav rad koji smo uložili je
+                            pro-bono. Izvorni kod se u svakom trenutku može vidjeti <a
+                                    href="https://github.com/tokict/humanitarci">ovdje</a>.
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</section>
+<!-- End About Section -->
+
+<!-- Divider -->
+<hr class="mt-0 mb-0 "/>
+<!-- End Divider -->
+
+<!-- Newsletter Section -->
+<section class="page-section bg-dark" data-background="front/images/djeca.jpg" id="subscribe">
+    <div class="container relative">
+
+        <form class="form align-center" id="mailchimp">
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+
+                    <div class="newsletter-label font-alt">
+                        Želite li da Vam javimo kad servis bude spreman?
+                    </div>
+
+                    <div class="mb-20">
+                        <input placeholder="Email adresa" class="newsletter-field form-control input-md round mb-xs-10"
+                               type="email" pattern=".{5,100}" required/>
+
+                        <button type="submit" class="btn btn-mod btn-w btn-medium btn-round mb-xs-10">
+                            Pošalji
+                        </button>
+                    </div>
+
+                    <div class="form-tip" style="color: white">
+                        <i class="fa fa-info-circle"></i> Ne bojte se, mi Vam nikada nećeemo slati spam!
+                    </div>
+
+                    <div id="subscribe-result"></div>
+
+                </div>
+            </div>
+        </form>
+
+    </div>
+</section>
+<!-- End Newsletter Section -->
+
+<!-- Foter -->
+<footer class="small-section bg-gray-lighter footer pb-60">
+    <div class="container">
+
+
+        <!-- Social Links -->
+        <div class="footer-social-links mb-50 mb-xs-50">
+            <a href="https://web.facebook.com/humanitarci.hr/?ref=bookmarks" title="Facebook" target="_blank"><i
+                        class="fa fa-facebook"></i></a>
+        </div>
+        <!-- End Social Links -->
+
+        <!-- Footer Text -->
+        <div class="footer-text">
+            <!-- Copyright -->
+            <div class="footer-copy font-alt">
+                <a href="http://themeforest.net/user/theme-guru/portfolio" target="_blank">© humanitarci.hr 2016</a>.
+            </div>
+            <!-- End Copyright -->
+            <div class="footer-made">
+                Da bismo lakše pomagali jedni drugima.
+            </div>
+        </div>
+        <!-- End Footer Text -->
+
+    </div>
+
+
+    <!-- Top Link -->
+    <div class="local-scroll">
+        <a href="#top" class="link-to-top"><i class="fa fa-caret-up"></i></a>
+    </div>
+    <!-- End Top Link -->
+
+</footer>
+<!-- End Foter -->
+
+
+</div>
+<!-- End Page Wrap -->
+
+
+<!-- JS -->
+<script type="text/javascript" src="front/js/jquery-1.11.2.min.js"></script>
+<script type="text/javascript" src="front/js/jquery.easing.1.3.js"></script>
+<script type="text/javascript" src="front/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="front/js/SmoothScroll.js"></script>
+<script type="text/javascript" src="front/js/jquery.scrollTo.min.js"></script>
+<script type="text/javascript" src="front/js/jquery.localScroll.min.js"></script>
+<script type="text/javascript" src="front/js/jquery.viewport.mini.js"></script>
+<script type="text/javascript" src="front/js/jquery.countTo.js"></script>
+<script type="text/javascript" src="front/js/jquery.appear.js"></script>
+<script type="text/javascript" src="front/js/jquery.sticky.js"></script>
+<script type="text/javascript" src="front/js/jquery.parallax-1.1.3.js"></script>
+<script type="text/javascript" src="front/js/jquery.fitvids.js"></script>
+<script type="text/javascript" src="front/js/owl.carousel.min.js"></script>
+<script type="text/javascript" src="front/js/isotope.pkgd.min.js"></script>
+<script type="text/javascript" src="front/js/imagesloaded.pkgd.min.js"></script>
+<script type="text/javascript" src="front/js/jquery.magnific-popup.min.js"></script>
+<!-- Replace test API Key "AIzaSyAZsDkJFLS0b59q7cmW0EprwfcfUA8d9dg" with your own one below
+**** You can get API Key here - https://developers.google.com/maps/documentation/javascript/get-api-key -->
+<script type="text/javascript" src="front/js/maps.js"></script>
+<script type="text/javascript" src="front/js/gmap3.min.js"></script>
+<script type="text/javascript" src="front/js/wow.min.js"></script>
+<script type="text/javascript" src="front/js/masonry.pkgd.min.js"></script>
+<script type="text/javascript" src="front/js/jquery.simple-text-rotator.min.js"></script>
+<script type="text/javascript" src="front/js/all.js"></script>
+<script type="text/javascript" src="front/js/contact-form.js"></script>
+<script type="text/javascript" src="front/js/jquery.ajaxchimp.min.js"></script>
+<!--[if lt IE 10]>
+<script type="text/javascript" src="front/js/placeholder.js"></script><![endif]-->
+
+
+<script aria-hidden="true" type="application/x-lastpass" id="hiddenlpsubmitdiv" style="display: none;"></script>
+<script>try {
+        (function () {
+            for (var lastpass_iter = 0; lastpass_iter < document.forms.length; lastpass_iter++) {
+                var lastpass_f = document.forms[lastpass_iter];
+                if (typeof(lastpass_f.lpsubmitorig2) == "undefined") {
+                    lastpass_f.lpsubmitorig2 = lastpass_f.submit;
+                    if (typeof(lastpass_f.lpsubmitorig2) == 'object') {
+                        continue;
+                    }
+                    lastpass_f.submit = function () {
+                        var form = this;
+                        var customEvent = document.createEvent("Event");
+                        customEvent.initEvent("lpCustomEvent", true, true);
+                        var d = document.getElementById("hiddenlpsubmitdiv");
+                        if (d) {
+                            for (var i = 0; i < document.forms.length; i++) {
+                                if (document.forms[i] == form) {
+                                    if (typeof(d.innerText) != 'undefined') {
+                                        d.innerText = i.toString();
+                                    } else {
+                                        d.textContent = i.toString();
+                                    }
+                                }
+                            }
+                            d.dispatchEvent(customEvent);
+                        }
+                        form.lpsubmitorig2();
+                    }
+                }
+            }
+        })()
+    } catch (e) {
+    }</script>
+<script>try {
+        function lpshowmenudiv(id) {
+            closelpmenus(id);
+            var div = document.getElementById('lppopup' + id);
+            var btn = document.getElementById('lp' + id);
+            if (btn && div) {
+                var btnstyle = window.getComputedStyle(btn, null);
+                var divstyle = window.getComputedStyle(div, null);
+                var posx = btn.offsetLeft;
+                posx -= 80;
+                var divwidth = parseInt(divstyle.getPropertyValue('width'));
+                if (posx + divwidth > window.innerWidth - 25) {
+                    posx -= ((posx + divwidth) - window.innerWidth + 25);
+                }
+                div.style.left = posx + "px";
+                div.style.top = (btn.offsetTop + parseInt(btnstyle.getPropertyValue('height'))) + "px";
+                if (div.style.display == 'block') {
+                    div.style.display = 'none';
+                    if (typeof(slideup) == 'function') {
+                        slideup();
+                    }
+                } else div.style.display = 'block';
+            }
+        }
+
+        function closelpmenus(id) {
+            if (typeof(lpgblmenus) != 'undefined') {
+                for (var i = 0; i < lpgblmenus.length; i++) {
+                    if ((id == null || lpgblmenus[i] != 'lppopup' + id) && document.getElementById(lpgblmenus[i]))         document.getElementById(lpgblmenus[i]).style.display = 'none';
+                }
+            }
+        }
+
+        var lpcustomEvent = document.createEvent('Event');
+        lpcustomEvent.initEvent('lpCustomEventMenu', true, true);
+    } catch (e) {
+    }</script>
+<script>try {
+        document.addEventListener('mouseup', function (e) {
+            if (typeof(closelpmenus) == 'function') {
+                closelpmenus();
+            }
+        }, false)
+    } catch (e) {
+    }</script>
+<script type="text/javascript" src="front/js/jquery.downCount.js"></script>
+
+</body>
 </html>
