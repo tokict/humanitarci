0900e90b5b464143aaebd298e74791cb3e2dc00f
Tino Tokic <tino.tokic@credissimo.bg>
2016-11-04 10:38:34 +0200

    more initial setup with css and translations

---
+++ app/Http/Terranet/Administrator/Modules/LegalEntities.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Terranet\Administrator\Modules;
+
+use App\User;
+use Terranet\Administrator\Contracts\Module\Editable;
+use Terranet\Administrator\Contracts\Module\Exportable;
+use Terranet\Administrator\Contracts\Module\Filtrable;
+use Terranet\Administrator\Contracts\Module\Navigable;
+use Terranet\Administrator\Contracts\Module\Sortable;
+use Terranet\Administrator\Contracts\Module\Validable;
+use Terranet\Administrator\Resource;
+use Terranet\Administrator\Traits\Module\AllowFormats;
+use Terranet\Administrator\Traits\Module\HasFilters;
+use Terranet\Administrator\Traits\Module\HasForm;
+use Terranet\Administrator\Traits\Module\HasSortable;
+use Terranet\Administrator\Traits\Module\ValidatesForm;
+
+/**
+ * Administrator Users Module
+ *
+ * @package Terranet\Administrator
+ */
+class LegalEntities extends Resource implements Navigable, Filtrable, Editable, Validable, Sortable, Exportable
+{
+    use HasFilters, HasForm, HasSortable, ValidatesForm, AllowFormats;
+
+    /**
+     * The module Eloquent model
+     *
+     * @var string
+     */
+    protected $model = \App\Models\LegalEntity::class;
+
+    public function title()
+    {
+        return trans("administrator::module.resources.legal_entities");
+    }
+
+    public function group()
+    {
+        return trans('administrator::module.groups.legal_entities');
+    }
+
+    public function linkAttributes()
+    {
+        return ['icon' => 'fa fa-user'];
+    }
+}
--- resources/views/vendor/administrator/layouts/app.blade.php
+++ resources/views/vendor/administrator/layouts/app.blade.php
@@ -191,44 +191,6 @@
             </nav>
         </div>
         <div class="row  border-bottom white-bg dashboard-header">
-
-            <div class="col-md-3">
-                <h2>Welcome Amelia</h2>
-                <small>You have 42 messages and 6 notifications.</small>
-                <ul class="list-group clear-list m-t">
-                    <li class="list-group-item fist-item">
-                                <span class="pull-right">
-                                    09:00 pm
-                                </span>
-                        <span class="label label-success">1</span> Please contact me
-                    </li>
-                    <li class="list-group-item">
-                                <span class="pull-right">
-                                    10:16 am
-                                </span>
-                        <span class="label label-info">2</span> Sign a contract
-                    </li>
-                    <li class="list-group-item">
-                                <span class="pull-right">
-                                    08:22 pm
-                                </span>
-                        <span class="label label-primary">3</span> Open new shop
-                    </li>
-                    <li class="list-group-item">
-                                <span class="pull-right">
-                                    11:06 pm
-                                </span>
-                        <span class="label label-default">4</span> Call back to Sylvia
-                    </li>
-                    <li class="list-group-item">
-                                <span class="pull-right">
-                                    12:00 am
-                                </span>
-                        <span class="label label-primary">5</span> Write a letter to Sandra
-                    </li>
-                </ul>
-            </div>
-
             @if ($module)
 
                 @yield('scaffold.create')
@@ -263,49 +225,42 @@
 
 </div>
 
-<!-- Mainly scripts -->
-<script src="js/jquery-2.1.1.js"></script>
-<script src="js/bootstrap.min.js"></script>
-<script src="js/plugins/metisMenu/jquery.metisMenu.js"></script>
-<script src="js/plugins/slimscroll/jquery.slimscroll.min.js"></script>
+
 
 <!-- Flot -->
-<script src="js/plugins/flot/jquery.flot.js"></script>
-<script src="js/plugins/flot/jquery.flot.tooltip.min.js"></script>
-<script src="js/plugins/flot/jquery.flot.spline.js"></script>
-<script src="js/plugins/flot/jquery.flot.resize.js"></script>
-<script src="js/plugins/flot/jquery.flot.pie.js"></script>
+<script src="/administrator/js/plugins/flot/jquery.flot.js"></script>
+<script src="/administrator/js/plugins/flot/jquery.flot.tooltip.min.js"></script>
+<script src="/administrator/js/plugins/flot/jquery.flot.spline.js"></script>
+<script src="/administrator/js/plugins/flot/jquery.flot.resize.js"></script>
+<script src="/administrator/js/plugins/flot/jquery.flot.pie.js"></script>
 
 <!-- Peity -->
-<script src="js/plugins/peity/jquery.peity.min.js"></script>
-<script src="js/demo/peity-demo.js"></script>
+<script src="/administrator/js/plugins/peity/jquery.peity.min.js"></script>
+
 
 <!-- Custom and plugin javascript -->
-<script src="js/inspinia.js"></script>
-<script src="js/plugins/pace/pace.min.js"></script>
+
+
 
 <!-- jQuery UI -->
-<script src="js/plugins/jquery-ui/jquery-ui.min.js"></script>
+<script src="/administrator/js/plugins/jquery-ui/jquery-ui.min.js"></script>
 
 <!-- GITTER -->
-<script src="js/plugins/gritter/jquery.gritter.min.js"></script>
+<script src="/administrator/js/plugins/gritter/jquery.gritter.min.js"></script>
 
 <!-- Sparkline -->
-<script src="js/plugins/sparkline/jquery.sparkline.min.js"></script>
-
-<!-- Sparkline demo data  -->
-<script src="js/demo/sparkline-demo.js"></script>
+<script src="/administrator/js/plugins/sparkline/jquery.sparkline.min.js"></script>
 
 <!-- ChartJS-->
-<script src="js/plugins/chartJs/Chart.min.js"></script>
+<script src="/administrator/js/plugins/chartJs/Chart.min.js"></script>
 
 <!-- Toastr -->
-<script src="js/plugins/toastr/toastr.min.js"></script>
+<script src="/administrator/js/plugins/toastr/toastr.min.js"></script>
 
 
 <script>
     $(document).ready(function() {
-        setTimeout(function() {
+        setTimeout(function () {
             toastr.options = {
                 closeButton: true,
                 progressBar: true,
@@ -315,93 +270,8 @@
             toastr.success('Responsive Admin Theme', 'Welcome to INSPINIA');
 
         }, 1300);
-
-
-        var data1 = [
-            [0,4],[1,8],[2,5],[3,10],[4,4],[5,16],[6,5],[7,11],[8,6],[9,11],[10,30],[11,10],[12,13],[13,4],[14,3],[15,3],[16,6]
-        ];
-        var data2 = [
-            [0,1],[1,0],[2,2],[3,0],[4,1],[5,3],[6,1],[7,5],[8,2],[9,3],[10,2],[11,1],[12,0],[13,2],[14,8],[15,0],[16,0]
-        ];
-        $("#flot-dashboard-chart").length && $.plot($("#flot-dashboard-chart"), [
-                    data1, data2
-                ],
-                {
-                    series: {
-                        lines: {
-                            show: false,
-                            fill: true
-                        },
-                        splines: {
-                            show: true,
-                            tension: 0.4,
-                            lineWidth: 1,
-                            fill: 0.4
-                        },
-                        points: {
-                            radius: 0,
-                            show: true
-                        },
-                        shadowSize: 2
-                    },
-                    grid: {
-                        hoverable: true,
-                        clickable: true,
-                        tickColor: "#d5d5d5",
-                        borderWidth: 1,
-                        color: '#d5d5d5'
-                    },
-                    colors: ["#1ab394", "#1C84C6"],
-                    xaxis:{
-                    },
-                    yaxis: {
-                        ticks: 4
-                    },
-                    tooltip: false
-                }
-        );
-
-        var doughnutData = {
-            labels: ["App","Software","Laptop" ],
-            datasets: [{
-                data: [300,50,100],
-                backgroundColor: ["#a3e1d4","#dedede","#9CC3DA"]
-            }]
-        } ;
-
-
-        var doughnutOptions = {
-            responsive: false,
-            legend: {
-                display: false
-            }
-        };
-
-
-        var ctx4 = document.getElementById("doughnutChart").getContext("2d");
-        new Chart(ctx4, {type: 'doughnut', data: doughnutData, options:doughnutOptions});
-
-        var doughnutData = {
-            labels: ["App","Software","Laptop" ],
-            datasets: [{
-                data: [70,27,85],
-                backgroundColor: ["#a3e1d4","#dedede","#9CC3DA"]
-            }]
-        } ;
-
-
-        var doughnutOptions = {
-            responsive: false,
-            legend: {
-                display: false
-            }
-        };
-
-
-        var ctx4 = document.getElementById("doughnutChart2").getContext("2d");
-        new Chart(ctx4, {type: 'doughnut', data: doughnutData, options:doughnutOptions});
-
     });
+
 </script>
 </body>
 {{--<div id="wrapper">
