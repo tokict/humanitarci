06ff34ff92e09f818e2d51838e5826e06e6b2298
Tino Tokic <tino.tokic@credissimo.bg>
2016-11-04 11:51:45 +0200

    Remove duplicated body and js

--- resources/views/vendor/administrator/layouts/app.blade.php
+++ resources/views/vendor/administrator/layouts/app.blade.php
@@ -60,7 +60,6 @@
     @yield('scaffold.headjs')
 </head>
 <body>
-<body>
 <div id="wrapper">
     @include($template->menu('sidebar'))
 
@@ -71,228 +70,14 @@
 
                     <form role="search" class="navbar-form-custom" action="search_results.html">
                         <div class="form-group">
-                            <input type="text" placeholder="Search for something..." class="form-control" name="top-search" id="top-search">
+                            <input type="text" placeholder="Search for something..." class="form-control"
+                                   name="top-search" id="top-search">
                         </div>
                     </form>
                 </div>
                 <ul class="nav navbar-top-links navbar-right">
                     <li>
                         <span class="m-r-sm text-muted welcome-message">Welcome to Humanitarci.hr</span>
-                    </li>
-                    <li class="dropdown">
-                        <a class="dropdown-toggle count-info" data-toggle="dropdown" href="#">
-                            <i class="fa fa-envelope"></i>  <span class="label label-warning">16</span>
-                        </a>
-                        <ul class="dropdown-menu dropdown-messages">
-                            <li>
-                                <div class="dropdown-messages-box">
-                                    <a href="profile.html" class="pull-left">
-                                        <img alt="image" class="img-circle" src="img/a7.jpg">
-                                    </a>
-                                    <div class="media-body">
-                                        <small class="pull-right">46h ago</small>
-                                        <strong>Mike Loreipsum</strong> started following <strong>Monica Smith</strong>. <br>
-                                        <small class="text-muted">3 days ago at 7:58 pm - 10.06.2014</small>
-                                    </div>
-                                </div>
-                            </li>
-                            <li class="divider"></li>
-                            <li>
-                                <div class="dropdown-messages-box">
-                                    <a href="profile.html" class="pull-left">
-                                        <img alt="image" class="img-circle" src="img/a4.jpg">
-                                    </a>
-                                    <div class="media-body ">
-                                        <small class="pull-right text-navy">5h ago</small>
-                                        <strong>Chris Johnatan Overtunk</strong> started following <strong>Monica Smith</strong>. <br>
-                                        <small class="text-muted">Yesterday 1:21 pm - 11.06.2014</small>
-                                    </div>
-                                </div>
-                            </li>
-                            <li class="divider"></li>
-                            <li>
-                                <div class="dropdown-messages-box">
-                                    <a href="profile.html" class="pull-left">
-                                        <img alt="image" class="img-circle" src="img/profile.jpg">
-                                    </a>
-                                    <div class="media-body ">
-                                        <small class="pull-right">23h ago</small>
-                                        <strong>Monica Smith</strong> love <strong>Kim Smith</strong>. <br>
-                                        <small class="text-muted">2 days ago at 2:30 am - 11.06.2014</small>
-                                    </div>
-                                </div>
-                            </li>
-                            <li class="divider"></li>
-                            <li>
-                                <div class="text-center link-block">
-                                    <a href="mailbox.html">
-                                        <i class="fa fa-envelope"></i> <strong>Read All Messages</strong>
-                                    </a>
-                                </div>
-                            </li>
-                        </ul>
-                    </li>
-                    <li class="dropdown">
-                        <a class="dropdown-toggle count-info" data-toggle="dropdown" href="#">
-                            <i class="fa fa-bell"></i>  <span class="label label-primary">8</span>
-                        </a>
-                        <ul class="dropdown-menu dropdown-alerts">
-                            <li>
-                                <a href="mailbox.html">
-                                    <div>
-                                        <i class="fa fa-envelope fa-fw"></i> You have 16 messages
-                                        <span class="pull-right text-muted small">4 minutes ago</span>
-                                    </div>
-                                </a>
-                            </li>
-                            <li class="divider"></li>
-                            <li>
-                                <a href="profile.html">
-                                    <div>
-                                        <i class="fa fa-twitter fa-fw"></i> 3 New Followers
-                                        <span class="pull-right text-muted small">12 minutes ago</span>
-                                    </div>
-                                </a>
-                            </li>
-                            <li class="divider"></li>
-                            <li>
-                                <a href="grid_options.html">
-                                    <div>
-                                        <i class="fa fa-upload fa-fw"></i> Server Rebooted
-                                        <span class="pull-right text-muted small">4 minutes ago</span>
-                                    </div>
-                                </a>
-                            </li>
-                            <li class="divider"></li>
-                            <li>
-                                <div class="text-center link-block">
-                                    <a href="notifications.html">
-                                        <strong>See All Alerts</strong>
-                                        <i class="fa fa-angle-right"></i>
-                                    </a>
-                                </div>
-                            </li>
-                        </ul>
-                    </li>
-
-
-                    <li>
-                        <a href="login.html">
-                            <i class="fa fa-sign-out"></i> Log out
-                        </a>
-                    </li>
-                    <li>
-                        <a class="right-sidebar-toggle">
-                            <i class="fa fa-tasks"></i>
-                        </a>
-                    </li>
-                </ul>
-
-            </nav>
-        </div>
-        <div class="row  border-bottom white-bg dashboard-header">
-            @if ($module)
-
-                @yield('scaffold.create')
-
-                <div style="float:none; clear: left;">
-                    @include($template->partials('breadcrumbs'))
-                </div>
-
-
-            @endif
-
-            <div class="box">
-                @yield('scaffold.filter')
-
-                <div class="box-body">
-                    @yield('scaffold.content')
-                </div>
-
-                <div class="box-footer">
-                    @yield('scaffold.content-footer')
-                </div>
-            </div>
-
-
-
-        </div>
-
-
-    </div>
-
-
-
-</div>
-
-
-
-<!-- Flot -->
-<script src="/administrator/js/plugins/flot/jquery.flot.js"></script>
-<script src="/administrator/js/plugins/flot/jquery.flot.tooltip.min.js"></script>
-<script src="/administrator/js/plugins/flot/jquery.flot.spline.js"></script>
-<script src="/administrator/js/plugins/flot/jquery.flot.resize.js"></script>
-<script src="/administrator/js/plugins/flot/jquery.flot.pie.js"></script>
-
-<!-- Peity -->
-<script src="/administrator/js/plugins/peity/jquery.peity.min.js"></script>
-
-
-<!-- Custom and plugin javascript -->
-
-
-
-<!-- jQuery UI -->
-<script src="/administrator/js/plugins/jquery-ui/jquery-ui.min.js"></script>
-
-<!-- GITTER -->
-<script src="/administrator/js/plugins/gritter/jquery.gritter.min.js"></script>
-
-<!-- Sparkline -->
-<script src="/administrator/js/plugins/sparkline/jquery.sparkline.min.js"></script>
-
-<!-- ChartJS-->
-<script src="/administrator/js/plugins/chartJs/Chart.min.js"></script>
-
-<!-- Toastr -->
-<script src="/administrator/js/plugins/toastr/toastr.min.js"></script>
-
-
-<script>
-    $(document).ready(function() {
-        setTimeout(function () {
-            toastr.options = {
-                closeButton: true,
-                progressBar: true,
-                showMethod: 'slideDown',
-                timeOut: 4000
-            };
-            toastr.success('Responsive Admin Theme', 'Welcome to INSPINIA');
-
-        }, 1300);
-    });
-
-</script>
-</body>
-{{--<div id="wrapper">
-
-    <div id="page-wrapper" class="gray-bg dashbard-1">
-        <div class="row border-bottom">
-            <nav class="navbar navbar-static-top" role="navigation" style="margin-bottom: 0">
-                <div class="navbar-header">
-                    <a class="navbar-minimalize minimalize-styl-2 btn btn-primary " href="#"><i class="fa fa-bars"></i>
-                    </a>
-                    <form role="search" class="navbar-form-custom" action="search_results.html">
-                        <div class="form-group">
-                            <input type="text" placeholder="Search for something..." class="form-control"
-                                   name="top-search"
-                                   id="top-search">
-                        </div>
-                    </form>
-                </div>
-                <ul class="nav navbar-top-links navbar-right">
-                    <li>
-                        <span class="m-r-sm text-muted welcome-message">Welcome to INSPINIA+ Admin Theme.</span>
                     </li>
                     <li class="dropdown">
                         <a class="dropdown-toggle count-info" data-toggle="dropdown" href="#">
@@ -321,8 +106,7 @@
                                     <div class="media-body ">
                                         <small class="pull-right text-navy">5h ago</small>
                                         <strong>Chris Johnatan Overtunk</strong> started following <strong>Monica
-                                            Smith</strong>.
-                                        <br>
+                                            Smith</strong>. <br>
                                         <small class="text-muted">Yesterday 1:21 pm - 11.06.2014</small>
                                     </div>
                                 </div>
@@ -408,12 +192,7 @@
 
             </nav>
         </div>
-        <!-- Left side column. contains the logo and sidebar -->
-
-
-        <!-- Right side column. Contains the navbar and content of the page -->
         <div class="row  border-bottom white-bg dashboard-header">
-            <!-- Content Header (Page header) -->
             @if ($module)
 
                 @yield('scaffold.create')
@@ -423,46 +202,62 @@
                 </div>
 
 
-                @endif
+            @endif
 
+            <div class="box">
+                @yield('scaffold.filter')
 
-                        <!-- Main content -->
-                <section class="content">
+                <div class="box-body">
+                    @yield('scaffold.content')
+                </div>
 
-                    <div class="box">
-                        @yield('scaffold.filter')
+                <div class="box-footer">
+                    @yield('scaffold.content-footer')
+                </div>
+            </div>
 
-                        <div class="box-body">
-                            @yield('scaffold.content')
-                        </div>
-
-                        <div class="box-footer">
-                            @yield('scaffold.content-footer')
-                        </div>
-                    </div>
-                    <!-- /.box -->
-                </section><!-- /.content -->
 
         </div>
-        <!-- /.content-wrapper -->
 
-        <footer class="main-footer">
-            <div class="pull-right hidden-xs">
-                <b>Version</b> 3.0
-            </div>
-            <strong>
-                Copyright &copy; {{ date('Y')-1 }} - {{ date('Y') }}
-                <a href="http://terranet.md">Terranet.md</a>.
-            </strong> All rights reserved.
-        </footer>
+
     </div>
-</div>--}}
-<!-- ./wrapper -->
+
+
+</div>
 
 <!-- jQuery 2.1.4 -->
 <script src="{{ asset($config->get('assets_path') . '/plugins/jQuery/jQuery-2.1.4.min.js') }}"></script>
 <!-- Bootstrap 3.3.5 -->
 <script src="{{ asset($config->get('assets_path') . '/bootstrap/js/bootstrap.min.js') }}"></script>
+<!-- Flot -->
+<script src="/administrator/js/plugins/flot/jquery.flot.js"></script>
+<script src="/administrator/js/plugins/flot/jquery.flot.tooltip.min.js"></script>
+<script src="/administrator/js/plugins/flot/jquery.flot.spline.js"></script>
+<script src="/administrator/js/plugins/flot/jquery.flot.resize.js"></script>
+<script src="/administrator/js/plugins/flot/jquery.flot.pie.js"></script>
+
+<!-- Peity -->
+<script src="/administrator/js/plugins/peity/jquery.peity.min.js"></script>
+
+
+<!-- Custom and plugin javascript -->
+
+
+<!-- jQuery UI -->
+<script src="/administrator/js/plugins/jquery-ui/jquery-ui.min.js"></script>
+
+<!-- GITTER -->
+<script src="/administrator/js/plugins/gritter/jquery.gritter.min.js"></script>
+
+<!-- Sparkline -->
+<script src="/administrator/js/plugins/sparkline/jquery.sparkline.min.js"></script>
+
+<!-- ChartJS-->
+<script src="/administrator/js/plugins/chartJs/Chart.min.js"></script>
+
+<!-- Toastr -->
+<script src="/administrator/js/plugins/toastr/toastr.min.js"></script>
+
 <script src="{{ asset($config->get('assets_path') . '/plugins/daterangepicker/moment.min.js') }}"></script>
 <script src="{{ asset($config->get('assets_path') . '/plugins/daterangepicker/daterangepicker.js') }}"></script>
 <!-- metisMenu -->
