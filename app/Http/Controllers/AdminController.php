9d16bede532f0888a9c52a900b9f578f8e440355
tino <tino@tokic.com.hr>
2016-11-05 18:34:25 +0200

    Added login and init dashboard from scratch

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use Illuminate\Support\Facades\View;
+
+class AdminController extends Controller
+{
+    public function index()
+    {
+        return view('admin.index');
+    }
+}
