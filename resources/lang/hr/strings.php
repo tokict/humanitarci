e8a7a77deacaed841168cea2f229922af15ca7ad
Tino Tokic <tino.tokic@credissimo.bg>
2016-11-25 16:51:12 +0200

    Change msg on successful payment

--- resources/lang/hr/strings.php
+++ resources/lang/hr/strings.php
@@ -12,6 +12,7 @@ return [
         'monthly' => 'mjesečna'
     ],
     'payment' => [
-        'Payment was successful! Your donation should be visible in no later than 24 hours after payment' => 'Plaćanje je uspješno izvršeno! Vaša donacija biti će vidljiva u roku ok 24 sata ukoliko nije već prisutna'
+        'Payment was successful! Your donation should be visible in no later than 24 hours after payment' =>
+            'Plaćanje je uspješno izvršeno! Vaša donacija biti će vidljiva najkasnije za 24 sata. Obavijestit cemo Vas mailom cim novac sjedne na nas racun.'
     ]
 ];
