7907560d59078144d408ed53d5f4091604256699
Tino Tokic <tino.tokic@credissimo.bg>
2017-03-29 15:42:29 +0300

    Change log location

--- config/app.php
+++ config/app.php
@@ -111,7 +111,7 @@ return [
     |
     */
 
-    'log' => env('APP_LOG', 'errorlog'),
+    'log' => env('APP_LOG', 'daily'),
     'log_max_files' => 60,
     'log_level' => env('APP_LOG_LEVEL', 'error'),
 
